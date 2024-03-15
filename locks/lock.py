53573524e8b7abccfa8d38bfd072b37fc5eca0f5
Anton <a.tashchilin@bk.ru>
2024-03-15 09:55:37 +0400

    fix bags and isp

--- locks/lock.py
+++ locks/lock.py
@@ -5,7 +5,7 @@ from util.util import ObjLock
 class Lock(ABC):
 
     @abstractmethod
-    def lock(self,resorce:str,ttl:int) -> ObjLock|False:
+    def lock(self,resorce:str,ttl:int) -> ObjLock|bool:
         pass
     @abstractmethod
     def unlock(self,resorce:ObjLock) -> None:
--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 
 
+from threading import Thread, get_native_id
+import time
 from data_readers.file_reader import FileReader
 from data_writers.file_writer import FileWriter
+from locks.redlock import Redlock
 
+class CommonResource:
+    x:int
+
+commRes = CommonResource()
+
+# def run(rl:Redlock):
+#     lock = rl.lock("app",1000)
+#     if not lock:
+#         return
+#     commRes.x = 0
+#     for _ in range(1,5):
+#         print(get_native_id(), commRes.x)
+#         commRes.x +=1
+#         time.sleep(100)
+#     rl.unlock(rl)
+        
+def run():
+    commRes.x = 0
+    for _ in range(1,5):
+        print(get_native_id(), commRes.x)
+        commRes.x +=1
+        # time.sleep(100)
 
 if __name__ == "__main__":
-    FileWriter("file").write("Hello world")
-    print(FileReader("file").read())
+    Thread(target=run).start()
+    Thread(target=run).start()
+    Thread(target=run).start()
+    
