ea9658d24c16c2f9c9e24e5ef5f03746c4e6f161
anton <a.tashchilin@bk.ru>
2024-03-17 22:13:58 +0400

    add change

--- scripts/start_cron_apps.sh
+++ scripts/start_cron_apps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker run -p 6379:6379 -d redis/redis-stack-server:latest
-echo "* * * * * $USER ./run.sh; $USER ./run.sh" > apps
+echo "* * * * * $USER $(pwd)/run.sh; $USER $(pwd)/run.sh" > apps
 crontab apps
