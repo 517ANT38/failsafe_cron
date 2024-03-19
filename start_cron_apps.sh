e283220e3920832b10c6f0848f759e68ce0c4556
anton <a.tashchilin@bk.ru>
2024-03-19 10:14:42 +0400

    add change

--- start_cron_apps.sh
+++ start_cron_apps.sh
@@ -3,5 +3,4 @@ docker run -p 6379:6379 -d redis/redis-stack-server:latest
 path_root="/home/anton/failsafe_cron"
 path_venv=".venv/bin/python"
 command="cd $path_root && $path_venv main.py"
-echo "* * * * *  $command;$command" > apps
-crontab apps
+echo "* * * * *  $command;$command" | crontab
