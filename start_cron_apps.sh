4dfbbdd674bbf7efc98b526bafc32e37f2dd9f4b
anton <a.tashchilin@bk.ru>
2024-03-19 10:48:42 +0400

    add else

--- start_cron_apps.sh
+++ start_cron_apps.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-CONTAINER_ID=$(docker ps -q -f name=redis)
+CONTAINER_ID=$(docker ps -a -q -f name=redis)
 PATH_ROOT="/home/anton/failsafe_cron"
 PATH_VENV=".venv/bin/python"
 COMMAND="cd $PATH_ROOT && $PATH_VENV main.py"
 
 if [ -z "$CONTAINER_ID" ]; then
-  docker run -p 6379:6379 --name redis -d redis/redis-stack-server:latest
+    docker run -p 6379:6379 --name redis -d redis/redis-stack-server:latest
+else
+    docker start redis
 fi
 
 echo -e "* * * * *  $COMMAND\n* * * * *  $COMMAND" | crontab
