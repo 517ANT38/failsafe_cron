536a4fdcc036d1147fdd7e279d3779aa8afc3d6a
anton <a.tashchilin@bk.ru>
2024-03-21 23:00:49 +0400

    add change

--- app/locks/redlock.py
+++ app/locks/redlock.py
@@ -9,36 +9,36 @@ from app.util.util import ObjLock
 
 class Redlock(Lock):
     
-    unlock_script = """
+    _unlock_script = """
     if redis.call("get",KEYS[1]) == ARGV[1] then
         return redis.call("del",KEYS[1])
     else
         return 0
     end"""
-    gerate_val:GenerateUnique
-    server_redis:Redis
+    _generate_val:GenerateUnique
+    _server_redis:Redis
     
-    def __init__(self,connect_info:str|dict):
+    def __init__(self,connect_info:str|dict,generate_val = SimpleGenerate()):
         
-        self.gerate_val = SimpleGenerate()
+        self._generate_val = generate_val
         if type(connect_info) == dict:
-            self.server_redis = StrictRedis(**connect_info)
+            self._server_redis = StrictRedis(**connect_info)
         else:
-            self.server_redis = StrictRedis.from_url(connect_info)
+            self._server_redis = StrictRedis.from_url(connect_info)
         
         
     def _release_instance(self, resource:str,key:str):        
-        self.server_redis.eval(self.unlock_script, 1, resource, key)
+        self._server_redis.eval(self._unlock_script, 1, resource, key)
         
         
     def _acquire_instance(self,resorce: str,val:str, ttl: int):
-        return self.server_redis.set(resorce,val,nx = True,px = ttl)        
+        return self._server_redis.set(resorce,val,nx = True,px = ttl)        
         
     
     
     def acquire(self, resorce: str, ttl: int):
         
-        val = self.gerate_val.generate(22)    
+        val = self._generate_val.generate(22)    
         try:
             flag = self._acquire_instance(resorce,val,ttl)
             if not flag:            
