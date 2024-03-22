c1b460e3eb7d6617bdc8103ada0abc945ade68c5
anton <a.tashchilin@bk.ru>
2024-03-22 07:38:53 +0400

    add change

--- app/locks/redlock.py
+++ app/locks/redlock.py
@@ -1,6 +1,6 @@
 
 import uuid
-from redis import RedisError, StrictRedis
+from redis import StrictRedis
 from app.exceptions.lock_exception import LockException
 from app.locks.lock import Lock
 
@@ -19,32 +19,31 @@ class Redlock(Lock):
     
     def __init__(self,connect_info:str|dict,resource_name:str,ttl:float):
         self._ttl = int(ttl*1000)
-        self._resource_name = resource_name
+        self._resource = resource_name
         if type(connect_info) == dict:
-            self._server_redis = StrictRedis(**connect_info)
+            self._redis = StrictRedis(**connect_info)
         else:
-            self._server_redis = StrictRedis.from_url(connect_info)
+            self._redis = StrictRedis.from_url(connect_info)
         
         
-    def _do_release(self, resource:str,token:str):        
-        self._server_redis.eval(self._unlock_script, 1, resource, token)
+    def _do_release(self,token:str):        
+        self._redis.eval(self._unlock_script, 1, self._resource, token)
         
         
-    def _do_acquire(self,resorce: str,token:str, ttl: int):
-        return self._server_redis.set(resorce,token,nx = True,px = ttl)        
+    def _do_acquire(self,token:str):
+        if self._redis.set(self._resource,token,nx = True,px = self._ttl):
+            return True
+        return False        
         
     
     
     def acquire(self):
         
-        token = uuid.uuid1().hex    
-        try:
-            flag = self._do_acquire(self._resource_name,token,self._ttl)
-            if not flag:            
-                raise LockException("It is not possible to take the lock")
-            self._token = token
-        except RedisError as e:
-            raise LockException("Redis error",e)
+        token = uuid.uuid1().hex  
+        if not self._do_acquire(token):            
+            raise LockException("It is not possible to take the lock")
+        self._token = token
+        
         
         
             
@@ -53,5 +52,5 @@ class Redlock(Lock):
         if expected_token is None:
             raise LockException("Cannot release an unlocked lock")
         self._token = None
-        self._do_release(self._resource_name,expected_token)
+        self._do_release(expected_token)
 
