836da724d1ef4201044d873b580eccf3b307e6d9
anton <a.tashchilin@bk.ru>
2024-03-22 08:43:41 +0400

    add change

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import logging
+from app.exceptions.lock_exception import LockException
 from app.starters.starter_worker_file import StarterWorkerFile
 
 if __name__ == "__main__":
-    
-    StarterWorkerFile("file.txt","redis://localhost:6379").run()    
+    logging.basicConfig(level=logging.DEBUG,filename="file_log.log")
+    try:
+        StarterWorkerFile("file.txt","redis://localhost:6379").run()  
+        logging.debug("successful write to file")  
+    except LockException as e:
+        logging.debug("error write to file",e.msg)
     
