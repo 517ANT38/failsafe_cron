621ab336bb110f8047ea845f1d40d8902fede176
anton <a.tashchilin@bk.ru>
2024-03-23 15:19:58 +0400

    change logger

--- app/log/util.py
+++ app/log/util.py
@@ -1,7 +1,13 @@
 
 import logging
 
-FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+FORMAT = '%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s'
+
+
+class CustomAdapter(logging.LoggerAdapter):
+    def process(self, msg, kwargs):
+        my_context = kwargs.pop('pid', self.extra['pid'])
+        return 'process [%s] %s' % (my_context, msg), kwargs
 
 def get_file_handler(filename:str):
     file_handler = logging.FileHandler(filename)
@@ -9,10 +15,8 @@ def get_file_handler(filename:str):
     file_handler.setFormatter(logging.Formatter(FORMAT))
     return file_handler
 
-
-
-def get_logger(filename:str,name:str):
+def get_logger(filename:str,name:str=None):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(get_file_handler(filename))  
-    return logger
+    return CustomAdapter(logger, {"pid": None})
--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from app.exceptions.lock_exception import LockException
 from app.log.util import get_logger
 from app.starters.starter_worker_file import StarterWorkerFile
 
-
 logger = get_logger("file_log.log",__name__)
 if __name__ == "__main__":    
     try:
         StarterWorkerFile("file.txt","redis://localhost:6379").run()
-        logger.info("process [%s] successful write to file",os.getpid())           
+        logger.info("successful write to file",pid=os.getpid())           
     except LockException as e:
-        logger.error("process [%s] error write to file: %s",os.getpid(),e.msg)
-    
+        logger.error("error write file: %s",e.msg,pid=os.getpid())    
