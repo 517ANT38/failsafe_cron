537f6b3dbf6909156e9e1661d6f46536c88f5246
anton <a.tashchilin@bk.ru>
2024-03-23 14:31:19 +0400

    add change

--- start_cron_apps.sh
+++ start_cron_apps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CONTAINER_ID=$(docker ps -a -q -f name=redis)
-PATH_ROOT="$(pwd)"
+PATH_ROOT="~/failsafe_cron"
 PATH_VENV=".venv/bin/python"
 COMMAND="* * * * * cd $PATH_ROOT && $PATH_VENV main.py"
 
@@ -9,10 +9,6 @@ if [ -z "$CONTAINER_ID" ]; then
 else
     docker start $CONTAINER_ID
 fi
-if [[ $(crontab -l | grep -v ^# | grep -v '^$' | wc -l) -eq 0 ]]; then
-    echo "$COMMAND" | crontab -
-else
-    (crontab -l ; echo "$COMMAND") | crontab -
-fi
-echo "$COMMAND" | crontab -
+
+(crontab -l &> /dev/null && (crontab -l ; echo "$COMMAND")) || echo "$COMMAND" | crontab - 
 (crontab -l ; echo "$COMMAND") | crontab -
