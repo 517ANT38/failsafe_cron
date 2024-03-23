cfe584d9602ce869279ab3199ee459d5e242ea05
anton <a.tashchilin@bk.ru>
2024-03-23 16:05:23 +0400

    add change and add file

--- scripts/start_cron_apps.sh
+++ scripts/start_cron_apps.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
-CONTAINER_ID=$(docker ps -a -q -f name=redis)
 PATH_ROOT="~/failsafe_cron"
 PATH_VENV=".venv/bin/python"
 COMMAND="* * * * * cd $PATH_ROOT && $PATH_VENV main.py"
-
-if [ -z "$CONTAINER_ID" ]; then
-    docker run -p 6379:6379 --name redis -d redis/redis-stack-server:latest > /dev/null
-else
-    docker start $CONTAINER_ID > /dev/null
-fi
 
 (crontab -l &> /dev/null && (crontab -l ; echo "$COMMAND")) || echo "$COMMAND" | crontab - 
 (crontab -l ; echo "$COMMAND") | crontab -
---
+++ scripts/start_redis.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+GD=docker
+GU=$(groups "$USER" | grep -o -w "\b$GD\b")
+
+if [ "$GD" = "$GR" ]; then
+    CONTAINER_ID=$(docker ps -a -q -f name=redis)
+    if [ -z "$CONTAINER_ID" ]; then
+        docker run -p 6379:6379 --name redis -d redis/redis-stack-server:latest > /dev/null
+    else
+        docker start $CONTAINER_ID > /dev/null
+    fi
+else
+    echo "User $USER is not a member of the Docker group."
+    exit 1
+fi
