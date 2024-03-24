1f792f688fdec8cce25762511dcf3a75872a6e4d
anton <a.tashchilin@bk.ru>
2024-03-24 14:08:22 +0400

    change redlock

--- app/locks/lock.py
+++ app/locks/lock.py
@@ -1,12 +1,13 @@
 from abc import ABC,abstractmethod
+from collections import namedtuple
 
-
+LockObj = namedtuple("Lock", ("resource", "token"))
 
 class Lock(ABC):
 
     @abstractmethod
-    def acquire(self) -> None:
+    def acquire(self,resource_name:str,ttl:float) -> LockObj:
         pass
     @abstractmethod
-    def release(self) -> None:
+    def release(self,lock_obj:LockObj) -> None:
         pass
--- app/locks/redlock.py
+++ app/locks/redlock.py
@@ -2,7 +2,7 @@
 import uuid
 from redis import StrictRedis
 from app.exceptions.lock_exception import LockException
-from app.locks.lock import Lock
+from app.locks.lock import Lock, LockObj
 
 
 
@@ -17,39 +17,35 @@ class Redlock(Lock):
    
     
     
-    def __init__(self,connect_info:str|dict,resource_name:str,ttl:float):
-        self._ttl = int(ttl*1000)
-        self._resource = resource_name
+    def __init__(self,connect_info:str|dict):
+        
         if type(connect_info) == dict:
             self._redis = StrictRedis(**connect_info)
         else:
             self._redis = StrictRedis.from_url(connect_info)
         
         
-    def _do_release(self,token:str):        
-        self._redis.eval(self._unlock_script, 1, self._resource, token)
+    def _do_release(self,resource:str,token:str):        
+        self._redis.eval(self._unlock_script, 1, resource, token)
         
         
-    def _do_acquire(self,token:str):
-        if self._redis.set(self._resource,token,nx = True,px = self._ttl):
+    def _do_acquire(self,token:str,resource:str,ttl:int):
+        if self._redis.set(resource,token,nx = True,px = ttl):
             return True
         return False        
         
     
     
-    def acquire(self):        
+    def acquire(self,resource_name:str,ttl:float):        
         token = uuid.uuid1().hex.encode()  
-        if not self._do_acquire(token):            
+        ttlMl = int(ttl * 1000)
+        if not self._do_acquire(token,resource_name,ttlMl):            
             raise LockException("It is not possible to take the lock")
-        self._token = token
+        return LockObj(resource_name,token)
         
         
         
             
-    def release(self):
-        expected_token = self._token
-        if expected_token is None:
-            raise LockException("Cannot release an unlocked lock")
-        self._token = None
-        self._do_release(expected_token)
+    def release(self,lock_obj:LockObj):        
+        self._do_release(lock_obj.resource,lock_obj.token)
 
